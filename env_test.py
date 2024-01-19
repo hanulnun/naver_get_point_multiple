257edba70837280aae1460d01390a49ae531bec0
hansu <hansu@duck.com>
2024-01-19 16:45:35 +0900

    환경 변수 파일로 접근

--- env_test.py
+++ env_test.py
@@ -2,8 +2,14 @@ import os
 
 
 if __name__ == "__main__":
+  
+
   print("환경변수 가져오기")
   a = os.environ.get("NAVER_TEST_ENV_VALUE")
   print(a)
   print("환경변수 저장하기")
-  os.system(f'echo \'NAVER_TEST_ENV_VALUE=N\' >> $GITHUB_ENV')
+  env_file = os.getenv('GITHUB_ENV')
+  with open(env_file, "a") as myfile:
+    myfile.write("NAVER_TEST_ENV_VALUE=N")
+  
+  # os.system(f'echo \'NAVER_TEST_ENV_VALUE=N\' >> $GITHUB_ENV')
